3da1aa139ab9c65b88d102c601ae44cbcbad67da
Valstion <lordvalstion@gmail.com>
2023-05-29 16:35:03 +0300

    Main 7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,138 @@
+import java.util.Random;
+
 public class Main {
+
+
+    public static void sumAndPrint(int a, int b) {
+        int result = a + b;
+        System.out.println("Suma: " + result);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 5;
+        int y = 3;
+        int d = 54;
+        int n = 32;
+        int min = 1;
+        int max = 10;
+        sumAndPrint(x, y);
+        int length1 = 5;
+        int min1 = 1;
+        int max1 = 10;
+        int rows = 4;
+        int columns = 6;
+
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(PISq());
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        int product = duTipoKintamuosios(d, n);
+        System.out.println(product);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        int[] myArray = {1, 2, 3, 4, 5};
+        printArrayElements(myArray);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        int randomNum = kuriSugeneruojaDuInt(min, max);
+        System.out.println("Atsitiktinis skaičius: " + randomNum);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        int[] randomNum1 = kurSkaiciuMaqsyva(min1, max1, length1);
+        for (int num : randomNum1) {
+            System.out.println(num);
+        }
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        int sum = funkcijaSusumojaMasyva(randomNum1);
+        System.out.println("Masyvo elementu suma: " + sum);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        double vidurkis = funkcijaSkaiciojaVidurki(randomNum1);
+        System.out.println("Masyvo elementu vidurkis: " + vidurkis);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        printRectangle(rows, columns);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        String sentence = "Šiandien labai graži diena";
+        countLettersAndSpaces(sentence);
+        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+        String encoded = reverseSentence(sentence);
+        System.out.println("Užkoduotas sakinys: " + encoded);
+
+    }
+
+    public static double PISq() {
+        double c = 9.8596;
+        return c;
+    }
+
+    public static int duTipoKintamuosios(int d, int n) {
+        int result = d * n;
+        System.out.println("Умножение " + result);
+        return result;
+    }
+
+    public static void printArrayElements(int[] arr) {
+        for (int num : arr) {
+            System.out.println(num);
+        }
+
+    }
+
+    public static int kuriSugeneruojaDuInt(int min, int max) {
+        Random rand = new Random();
+        return rand.nextInt(max - min + 1) + min;
+    }
+
+    public static int[] kurSkaiciuMaqsyva(int min1, int max1, int length1) {
+        int[] randomArray = new int[length1];
+        Random rand1 = new Random();
+        for (int i = 0; i < length1; i++) {
+            randomArray[i] = rand1.nextInt(max1 - min1 + 1) + min1;
+        }
+        return randomArray;
+    }
+
+    public static int funkcijaSusumojaMasyva(int[] massyvaS) {
+        int sum = 0;
+        for (int num : massyvaS) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static double funkcijaSkaiciojaVidurki(int[] massyvaS) {
+        int sum = 0;
+        for (int num : massyvaS) {
+            sum += num;
+        }
+        double vidurkis = (double) sum / massyvaS.length;
+
+        return vidurkis;
+    }
+
+    public static void printRectangle(int rows, int columns) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void countLettersAndSpaces(String sentence) {
+        int letterCount = 0;
+        int spaceCount = 0;
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (Character.isLetter(c)) {
+                letterCount++;
+            } else if (Character.isWhitespace(c)) {
+                spaceCount++;
+            }
+        }
+        System.out.println("Raidžių skaičius: " + letterCount);
+        System.out.println("Tarpų skaičius: " + spaceCount);
+    }
+
+    public static String reverseSentence(String sentence) {
+        StringBuilder reversed = new StringBuilder(sentence);
+        reversed.reverse();
+        return reversed.toString();
     }
 }
